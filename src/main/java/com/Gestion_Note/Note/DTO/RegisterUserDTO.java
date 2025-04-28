bd7a24dc8343a6c0af13fab9005ea1e6d37422a1
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-04-28 21:06:53 +0100

    Auth Request

---
+++ src/main/java/com/Gestion_Note/Note/DTO/RegisterUserDTO.java
@@ -0,0 +1,62 @@
+package com.Gestion_Note.Note.DTO;
+
+import com.Gestion_Note.Note.Entities.Roles;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterUserDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "Role is required")
+    private Roles role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
+}
